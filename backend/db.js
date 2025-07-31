44f3ebefb5f9c37366191982ddb01db33d3f5a1d
S1rDavid9 <d.akachi@alustudent.com>
2025-07-31 13:51:15 +0200

    Fix database connection to use Atlas in production

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://localhost:27017/codesensai';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/codesensai';
 
 const connectDB = async () => {
   try {
